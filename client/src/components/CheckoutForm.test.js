e7bddd722426c45e128cd220d8f9e18c16ef578d
Daniel Antonio <danieltomantonio@gamil.com>
2020-09-04 09:38:35 -0700

    Finished test script for CheckoutForm component

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    const firstNameInput = screen.getByLabelText(/First Name/i);
+    const lastNameInput = screen.getByLabelText(/Last Name/i);
+    const addressInput = screen.getByLabelText(/Address/i);
+    const cityInput = screen.getByLabelText(/City/i);
+    const stateInput = screen.getByLabelText(/State/i);
+    const zipInput = screen.getByLabelText(/Zip/i);
+    expect(firstNameInput).toBeInTheDocument();
+    expect(lastNameInput).toBeInTheDocument();
+    expect(addressInput).toBeInTheDocument();
+    expect(cityInput).toBeInTheDocument();
+    expect(stateInput).toBeInTheDocument();
+    expect(zipInput).toBeInTheDocument();
+
+    // Should fail
+    const firstNameInput = screen.not.getByLabelText(/First Name/i);
+});
