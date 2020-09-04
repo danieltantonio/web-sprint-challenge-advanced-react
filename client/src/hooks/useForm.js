198225d956af10e4d09981e5a19587c1894015ff
Daniel Antonio <danieltomantonio@gamil.com>
2020-09-04 09:17:09 -0700

    Created useForm hook and applied it to CheckoutForm Componenet

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+    const [formValues, setFormValues] = useState(initialValue);
+
+    const setValue = (value) => {
+        setFormValues(value)
+    };
+
+    return [formValues, setValue]
+};
